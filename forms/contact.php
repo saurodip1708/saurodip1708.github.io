8f13d250650bc56cb21187b858846c70f40ef31b
Saurodip Sinha <saurodip172005@gmail.com>
2021-08-18 20:32:45 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -37,5 +37,5 @@
   $contact->add_message( $_POST['email'], 'Email');
   $contact->add_message( $_POST['message'], 'Message', 10);
 
-  echo $contact->send();
+  echo $contact->send(saurodip172005@gmail.com);
 ?>
